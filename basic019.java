0bdfedf3d38404e2a3ec96ff259208850da6dc7e
cejinn <149019482+cejinn@users.noreply.github.com>
2023-11-05 00:00:41 +0900

    정수 찾기

---
+++ basic019.java
@@ -0,0 +1,11 @@
+// 정수 찾기
+
+class Solution {
+    public int solution(int[] num_list, int n) {
+        int answer = 0;
+        for(int a : num_list) {
+            if(a==n) {answer=1; break;}
+        }
+        return answer;
+    }
+}
