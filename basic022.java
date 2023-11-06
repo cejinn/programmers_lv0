ac4f84c7137f725584b09e579382a6c30a60e74c
cejinn <149019482+cejinn@users.noreply.github.com>
2023-11-07 08:43:25 +0900

    두 수의 차

--- basic022.java
+++ basic022.java
@@ -1,8 +1,8 @@
-// 몫 구하기
+// 두 수의 차
 
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = num1 / num2;
+        int answer = num1 - num2;
         return answer;
     }
 }
