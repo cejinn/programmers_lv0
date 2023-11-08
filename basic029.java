cffa84806b179169b4f4a37ab83564ed62da9581
cejinn <149019482+cejinn@users.noreply.github.com>
2023-11-09 05:56:18 +0900

    이어 붙인 수

---
+++ basic029.java
@@ -0,0 +1,14 @@
+// 이어 붙인 수
+
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        String odd ="", even="";
+        for(int i:num_list) {
+            if(i%2==0) {even+=String.valueOf(i);}
+            else {odd+=String.valueOf(i);}
+        }
+        answer = Integer.parseInt(odd)+Integer.parseInt(even);
+        return answer;
+    }
+}
