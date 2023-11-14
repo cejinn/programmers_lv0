303985918f9d8403d743c1e013feb3df34a9c047
cejinn <149019482+cejinn@users.noreply.github.com>
2023-11-15 07:12:00 +0900

    l로 만들기

---
+++ basic041.java
@@ -0,0 +1,11 @@
+// l로 만들기
+
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        for(int i=0; i<myString.length(); i++) {
+           answer += myString.charAt(i) < 'l'? 'l' : myString.charAt(i);
+        }
+        return answer;
+    }
+}
