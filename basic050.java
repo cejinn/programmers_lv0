f447e920902bd26fbc9ecf1d3ada6ee855de2587
cejinn <149019482+cejinn@users.noreply.github.com>
2023-11-21 09:01:14 +0900

    외계행성의 나이

---
+++ basic050.java
@@ -0,0 +1,24 @@
+// 외계행성의 나이
+
+class Solution {
+    public String solution(int age) {
+	    	String temp = "" + age;
+	        String answer = "";
+	        
+	        for(int i=0; i<temp.length(); i++) {
+	        	switch(temp.charAt(i)) {
+	        	case '0' : answer += "a"; break;
+	        	case '1' : answer += "b"; break;
+	        	case '2' : answer += "c"; break;
+	        	case '3' : answer += "d"; break;
+	        	case '4' : answer += "e"; break;
+	        	case '5' : answer += "f"; break;
+	        	case '6' : answer += "g"; break;
+	        	case '7' : answer += "h"; break;
+	        	case '8' : answer += "i"; break;
+	        	case '9' : answer += "j"; break;
+	        	}
+	        }
+	        return answer;
+    }
+}
